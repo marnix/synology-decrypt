0f4079d5390465bf7651085f946f82001997f087
Marnix Klooster <marnix.klooster+github.com@gmail.com>
2016-06-10 22:31:51 +0200

    Implemented basic __CLOUDSYNC_ENC__ file parsing

--- syndecrypt.py
+++ syndecrypt.py
@@ -1,6 +1,7 @@
 import pyaes
 import hashlib
 from passlib.utils.pbkdf2 import pbkdf1
+import struct
 
 
 # Thanks to http://security.stackexchange.com/a/117654/3617,
@@ -43,3 +44,72 @@ def decrypted_with_password(ciphertext, password):
         decrypted = decrypter.feed(ciphertext)
         decrypted += decrypter.feed()
         return decrypted
+
+
+def _read_objects_from(f):
+        result = []
+        while True:
+                obj = _read_object_from(f)
+                if obj == None: break
+                result += [obj]
+        return result
+
+def _read_object_from(f):
+        s = f.read(1)
+        if len(s) == 0: return None
+        header_byte = bytearray(s)[0]
+        if header_byte == 0x42:
+                return _continue_read_dict_from(f)
+        elif header_byte == 0x40:
+                return None
+        elif header_byte == 0x11:
+                return _continue_read_bytes_from(f)
+        elif header_byte == 0x10:
+                return _continue_read_string_from(f)
+        elif header_byte == 0x01:
+                return _continue_read_int_from(f)
+        else:
+                assert False, 'header_byte should not be ' + ("0x%02X" % header_byte)
+
+def _continue_read_dict_from(f):
+        result = {}
+        while True:
+                key = _read_object_from(f)
+                if key == None: break
+                value = _read_object_from(f)
+                result[key] = value
+        return result
+
+def _continue_read_bytes_from(f):
+        s = f.read(2)
+        length = struct.unpack('>H', s)[0]
+        return f.read(length)
+
+def _continue_read_string_from(f):
+        return _continue_read_bytes_from(f).decode('utf-8')
+
+def _continue_read_int_from(f):
+        s = f.read(1)
+        length = struct.unpack('>B', s)[0]
+        assert length == 1, 'currently only integers of 1 byte supported: endianness is unknown'
+        return ord(f.read(1))
+
+
+def decode_csenc_stream(f):
+        MAGIC = b'__CLOUDSYNC_ENC__'
+
+        s = f.read(len(MAGIC))
+        assert s == MAGIC, 'magic should not be ' + str(s) + ' but ' + str(MAGIC)
+        s = f.read(32)
+        magic_hash = hashlib.md5(MAGIC).hexdigest().encode('ascii')
+        assert s == magic_hash, 'magic hash should not be ' + str(s) + ' but ' + str(magic_hash)
+
+        metadata = {}
+        data = b''
+        for obj in _read_objects_from(f):
+                assert isinstance(obj, dict)
+                if obj['type'] == 'metadata':
+                        for (k,v) in obj.items():
+                                if k != 'type': yield (True, (k,v))
+                elif obj['type'] == 'data':
+                        yield (False, obj['data'])
--- test_decrypt.py
+++ test_decrypt.py
@@ -2,13 +2,13 @@ import syndecrypt
 
 import assertpy
 import base64
-import binascii
 
 
 def binary_contents_of(file_name):
         with open(file_name, 'rb') as f: return f.read()
 
 PASSWORD=binary_contents_of('testfiles-secrets/password.txt')
+
 
 def test_decrypt_enc_key1():
         """
@@ -21,3 +21,10 @@ def test_decrypt_enc_key1():
         enc_key1 = b'f662PyjwrkzR61qSRHyBEVkXVd7STUpV6o7IrJs+m8gN1haqmBtMzLvq2/Gj134r'
         enc_key1_binary = base64.b64decode(enc_key1)
         assert syndecrypt.decrypted_with_password(enc_key1_binary, PASSWORD) == b'BxY2A-ouRpI8YRvmiWii5KkCF3LVN1O6'
+
+
+def test_decode_single_line_file():
+        with open('testfiles-csenc/single-line.txt', 'rb') as f:
+                for (is_meta, value) in syndecrypt.decode_csenc_stream(f):
+                        if is_meta: print(value[0], ':', value[1])
+                        else: pass
